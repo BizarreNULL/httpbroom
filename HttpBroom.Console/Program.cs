f9161be60892d839689728157dd78980c1abfd87
Jojo <jonas.uliana@passwd.com.br>
2020-12-17 11:50:14 -0300

    Added HttpBroom.Console project

---
+++ HttpBroom.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HttpBroom.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
